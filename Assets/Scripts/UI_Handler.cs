4a3dd1cac8cb5fce1f747f3fc726113c35dc2573
CreatureMind <124280801+CreatureMind@users.noreply.github.com>
2025-05-19 13:33:41 +0300

    Added new prefabs and heart logic

--- Assets/Scripts/UI_Handler.cs
+++ Assets/Scripts/UI_Handler.cs
@@ -1,5 +1,8 @@
+using System;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UI_Handler : MonoBehaviour
 {
@@ -12,13 +15,45 @@ public class UI_Handler : MonoBehaviour
     [Header("<allcaps><u>Percentage:")]
     [SerializeField] private TMP_Text _percentText;
     [SerializeField, Range(0, 999)] private int maxPercent;
-    [Range(0, 100)] public int Percent;
     [SerializeField] private Color[] _percentColors;
     private float _currentFontSize;
 
+    [Header("<allcaps><u>Lives:")]
+    [SerializeField] private GameObject _heartImage;
+    [SerializeField] private List<GameObject> _heartImages;
+    [SerializeField] private GameObject _livesPanel;
+    [SerializeField] private int _maxLives;
+    
+    [Header("<allcaps><u>Popups:")]
+    [SerializeField] private GameObject _startPopup;
+    [SerializeField] private GameObject _endPopup;
+
+    private Camera _mainCamera;
+
+    private void OnEnable()
+    {
+        Unit.EnemyTookDamage += EnemyUIPercentageUpdate;
+        Unit.PlayerTookDamage.AddListener(PlayerUIPercentageUpdate);
+    }
+
+    private void OnDisable()
+    {
+        Unit.EnemyTookDamage -= EnemyUIPercentageUpdate;
+        Unit.PlayerTookDamage.RemoveListener(PlayerUIPercentageUpdate);
+    }
+
     private void Start()
     {
+        _mainCamera = Camera.main;
         _currentFontSize = _percentText.fontSize;
+
+        for (int i = 0; i < _maxLives; i++)
+        {
+            _heartImages.Add(Instantiate(_heartImage, _livesPanel.transform));
+            _heartImages[i].SetActive(false);
+        }
+
+        SetLives();
     }
 
     void FixedUpdate()
@@ -27,13 +62,13 @@ public class UI_Handler : MonoBehaviour
         if (Touch_Manager.InRadius)
         {
             Vector3 screenPosition = Touch_Manager.TouchPositionAction.ReadValue<Vector2>();
-            screenPosition.z = Vector3.Distance(Camera.main.transform.position, points[0].position);
+            screenPosition.z = Vector3.Distance(_mainCamera.transform.position, points[0].position);
 
             points[1].gameObject.SetActive(true);
 
-            Vector3 worldPosition = Camera.main.ScreenToWorldPoint(screenPosition);
+            Vector3 worldPosition = _mainCamera.ScreenToWorldPoint(screenPosition);
 
-            Vector3 offset = (worldPosition - points[0].position).normalized * _gizmoRadius * -1;
+            Vector3 offset = (worldPosition - points[0].position).normalized * (_gizmoRadius * -1);
             offset.y = 0;
 
             Vector3 targetPosition = points[0].position + offset;
@@ -47,23 +82,74 @@ public class UI_Handler : MonoBehaviour
         }
 
         line.SetUpLine(points);
-
-        UIPercentageUpdate();
     }
 
-    private void UIPercentageUpdate()
+    private void SetLives()
     {
-        int currentPercent, segmentCount = _percentColors.Length - 1;
+        for (int i = 0; i < Player_Manager.Instance.StatHandler.Health; i++)
+        {
+            _heartImages[i].SetActive(true);
+        }
+    }
+    private void RemoveHeart()
+    {
+        foreach (var heart in _heartImages)
+        {
+            if (heart.activeInHierarchy)
+            {
+                heart.SetActive(false);
+                break;
+            }
+        }
+    }
 
-        //_percentText.text = CurrentPercent.ToString() + "<size=60%>%";
-        _percentText.text = Percent.ToString() + "<size=60%>%";
+    private void AddHeart()
+    {
+        int i = 0;
+        foreach (var heart in _heartImages)
+        {
+            if (heart.activeInHierarchy)
+            {
+                i++;
+            }
+        }
 
-        currentPercent = Percent;
+        if (i >= _heartImages.Count)
+        {
+            _heartImages.Add(Instantiate(_heartImage, _livesPanel.transform));
+        }
+        else
+        {
+            _heartImages[i].SetActive(true);
+        }
+    }
+
+    private Color EnemyUIPercentageUpdate(int currentPercent)
+    {
+        int segmentCount = _percentColors.Length - 1;
+        
         float progress = Mathf.Abs((float)currentPercent / maxPercent * segmentCount);
         int segmentIndex = (int)Mathf.Floor(progress);
         float lerpValue = progress - segmentIndex;
-
-
+        
+        if(segmentIndex < _percentColors.Length - 1)
+        {
+            return Color.Lerp(_percentColors[segmentIndex], _percentColors[segmentIndex + 1], lerpValue);
+        }
+        
+        return _percentColors[^1];
+    }
+    
+    private void PlayerUIPercentageUpdate(int currentPercent)
+    {
+        int segmentCount = _percentColors.Length - 1;
+        
+        _percentText.text = currentPercent + "<size=60%>%";
+        
+        float progress = Mathf.Abs((float)currentPercent / maxPercent * segmentCount);
+        int segmentIndex = (int)Mathf.Floor(progress);
+        float lerpValue = progress - segmentIndex;
+        
         if(segmentIndex < _percentColors.Length - 1)
         {
             _percentText.color = Color.Lerp(_percentColors[segmentIndex], _percentColors[segmentIndex + 1], lerpValue);
@@ -71,7 +157,7 @@ public class UI_Handler : MonoBehaviour
         }
         else
         {
-            _percentText.color = _percentColors[_percentColors.Length - 1];
+            _percentText.color = _percentColors[^1];
             _percentText.fontSizeMax = _currentFontSize * 1.5f;
         }
     }
--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -1,11 +1,17 @@
 using System;
+using JetBrains.Annotations;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class Unit : MonoBehaviour
 {
     [SerializeField] protected Unit_Data unitData;
     public Unit_Data UnitData => unitData;
     [SerializeField] protected Rigidbody _rb;
+    [SerializeField] [CanBeNull] private Image percentImage;
+
     public Rigidbody Rigidbody => _rb;
     
     protected MovementState _movementState;
@@ -14,7 +20,8 @@ public class Unit : MonoBehaviour
     private int _currentPercent;
     public int CurrentPercent => _currentPercent;
 
-    public static Action<int> ITookDamage;
+    public static Func<int, Color> EnemyTookDamage;
+    public static UnityEvent<int> PlayerTookDamage = new();
     
     public void SetMovementState( MovementState state)
     { 
@@ -24,7 +31,15 @@ public class Unit : MonoBehaviour
     public void TakeDamage(int damage)
     {
         _currentPercent += damage;
-        ITookDamage?.Invoke( _currentPercent );
+        if (CompareTag("Player"))
+        {
+            PlayerTookDamage?.Invoke(_currentPercent);
+        }
+        else if (percentImage != null)
+        {
+            percentImage.color = EnemyTookDamage?.Invoke(_currentPercent) ?? Color.white;
+        }
+        
         Debug.Log($"{this} Taking Damage: {damage} CurrentPercent: {_currentPercent}");
     }
 
