970f94f4e13ac377b581cc54be61abd9bb226a5c
TheFull0 <joester17@gmail.com>
2025-06-06 15:56:32 +0300

    ui changes

---
+++ Assets/Scripts/UI/Main Menu/Button Tester.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class ButtonTester : MonoBehaviour
+{
+    public void OnButtonClick()
+    {
+        Debug.Log("Button clicked!");
+        // You can add more functionality here, like calling other methods or changing UI elements.
+    }
+}
