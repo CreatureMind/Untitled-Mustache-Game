e8a72c809433ef0a25e8a490b2181a74782000ff
TheFull0 <joester17@gmail.com>
2025-06-10 17:15:40 +0300

    json button creation

--- Assets/Scripts/UI/Menu Manager/Level_Select_Menu.cs
+++ Assets/Scripts/UI/Menu Manager/Level_Select_Menu.cs
@@ -1,37 +1,53 @@
-
 using System;
+using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Level_Select_Menu : Base_Menu
 {
-    [SerializeField] private Image emptyStar;
-    [SerializeField] private Image filledStar;
+    [SerializeField] private Sprite emptyStar;
+    [SerializeField] private Sprite filledStar;
     [SerializeField] private Button backButton;
+    
+    [SerializeField] private Transform levelButtonContainer;
+    
+    [SerializeField] private Level_Button levelButtonPrefabScript;
+    
+    [SerializeField] private string jsonPath = "Level_Button_Data.json";
 
     private void Awake()
     {
         backButton.onClick.AddListener(() => Menu_Manager.Instance.SwitchMenu(MenuState.Title));
+        
+        string path = Path.Combine(Application.streamingAssetsPath, jsonPath);
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            List<Level_Button_Data> buttonDataList = JsonHelper.FromJson<Level_Button_Data>(json);
+
+            foreach (Level_Button_Data buttonData in buttonDataList)
+            {
+                Level_Button newButton = Instantiate(levelButtonPrefabScript, levelButtonContainer);
+                newButton.InitalizeLevelButton(buttonData);    
+            }
+        }
     }
+    
+    
 }
 
 [Serializable]
-public class Level_Button
+public class Level_Button_Data
 {
-    private Button levelButton;
-    private Button normalDifficultyButton;
-    private Button infiniteDifficultyButton;
-
-    private bool isNormal;
-    
-    private bool isLocked;
-    private bool isInfiniteLocked;
-    
-    private Image mapImage;
-    
-    private Image star1;
-    private Image star2;
-    private Image star3;
-    //Game_Manager.Instance.StartLevel(levelIndex, difficulty);
-    
+    public string levelName;
+    public string pathToMapImage;
+    public int starsEarned;
+    LevelStateType levelStateType;
+}
+public enum LevelStateType
+{
+    Locked,
+    Normal,
+    Infinite
 }
