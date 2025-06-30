bd9ecba49397588c1d9d24eee7d803b60b134a9c
TheFull0 <joester17@gmail.com>
2025-06-30 17:41:41 +0300

    store buy button

--- Assets/Scripts/UI/Menu Manager/Store_Menu.cs
+++ Assets/Scripts/UI/Menu Manager/Store_Menu.cs
@@ -4,9 +4,15 @@ using UnityEngine.UI;
 public class Store_Menu : Base_Menu
 {
     [SerializeField] private Button backButton;
+    [SerializeField] private Button buyButton;
 
     private void Awake()
     {
         backButton.onClick.AddListener(() => Menu_Manager.Instance.SwitchMenu(MenuState.Title));
+        buyButton.onClick.AddListener(() => 
+        {
+            // Implement buy functionality here
+            Debug.Log("Buy button clicked");
+        });
     }
 }
